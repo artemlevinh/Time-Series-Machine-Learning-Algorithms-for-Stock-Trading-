b3b7adfd56902c588bcc6774ec6b059c5b50a44f
artemlevinh <88550828+artemlevinh@users.noreply.github.com>
2025-11-29 00:05:43 +0800

    Delete random_forest_use.py

--- random_forest_use.py
+++
@@ -1,8 +0,0 @@
-# random_forest_use.py
-import numpy as np
-import joblib
-
-def predict_next_price(last_10_prices, model_path="rf_model.pkl"):
-    model = joblib.load(model_path)
-    input_data = np.array(last_10_prices).reshape(1, -1)
-    return model.predict(input_data)[0]
